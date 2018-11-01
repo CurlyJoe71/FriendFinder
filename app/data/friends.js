affb90bd581b67383109fab038072a2e59d46300
Curly Joe <curlyjoe71@gmail.com>
2018-10-31 23:07:28 -0700

    Updating files

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friendArray = [
+    {
+        name:"Jesus 1",
+        photo:"https://randomassault.files.wordpress.com/2013/10/jesusthumbsup.jpg",
+        scores:[
+            4,
+            1,
+            5,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    },
+    {
+        name:"Jesus 2",
+        photo:"https://www.marypages.com/assets/images/ori/PrayerstoJesus.jpg",
+        scores:[
+            4,
+            2,
+            3,
+            5,
+            4,
+            4,
+            5,
+            3,
+            4,
+            5
+          ]
+    }
+  ];
+
+
+
+  module.exports = friendArray;
